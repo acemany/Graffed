cefc35fca9fdf13e90bbb92cbe02319da9213cf7
Acemany <116539353+acemany@users.noreply.github.com>
2024-10-16 02:22:51 +0300

    And also this

--- potaget.py
+++ potaget.py
@@ -115,7 +115,7 @@ def orient(c: Vector2, a: Vector2, b: Vector2):
 
 def c_c_c(cx1: float, cy1: float, cr1: float,
           cx2: float, cy2: float, cr2: float) -> bool:
-    return Vector2.distance_to((cx1, cy1), (cx2, cy2)) < cr1+cr2
+    return Vector2(cx1, cy1).distance_to((cx2, cy2)) < cr1+cr2
 
 
 def c_c_r(cx: float, cy: float, cr: float,
@@ -125,12 +125,12 @@ def c_c_r(cx: float, cy: float, cr: float,
 
 def c_r_r(bx1: float, by1: float, bw1: float, bh1: float,
           bx2: float, by2: float, bw2: float, bh2: float) -> bool:
-    return Rect.colliderect((bx1, by1, bw1, bh1), (bx2, by2, bw2, bh2))
+    return Rect(bx1, by1, bw1, bh1).colliderect((bx2, by2, bw2, bh2))
 
 
 def c_p_c(px: float, py: float,
           cx: float, cy: float, cr: float) -> bool:
-    return Vector2.distance_to((px, py), (cx, cy)) < cr
+    return Vector2(px, py).distance_to((cx, cy)) < cr
 
 
 def c_p_r(px: float, py: float,
